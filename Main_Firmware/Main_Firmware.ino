881896673791e882e3ebca89248e7b5ef1696d30
Elwin Hammer <elwinhammer@gmail.com>
2020-02-05 15:11:29 +0100

    First setup
    To include the 2 folders

---
+++ Main_Firmware/Main_Firmware.ino
@@ -0,0 +1,20 @@
+/*
+  #############################################################
+  #Firmware:           DigitalPlastic_DeltaRobot              #
+  #Hardware:           Arduino Mega 2560?                     #
+  #Eerste opzet:       05-02-2020                             #
+  #Auteurs: E. Hammer | N. Benhaddou | M. Faro | O. Cekem     #
+  #Laatst gewijzigd:   05-02-2020                             #
+  #Versie:             0.0.0                                  #
+  #############################################################
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
