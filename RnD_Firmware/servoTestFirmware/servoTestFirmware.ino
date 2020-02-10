76d67442311bd0589b32b06a60358757cc080961
Benhan <60701172+RandomPlutoPermanentCrystal@users.noreply.github.com>
2020-02-10 17:04:02 +0100

    demo functie
    demo functie toegevoegd #34

--- RnD_Firmware/servoTestFirmware/servoTestFirmware.ino
+++ RnD_Firmware/servoTestFirmware/servoTestFirmware.ino
@@ -1,9 +1,9 @@
 /*
   #############################################################
   #Firmware:           Servo Test Firmware                    #
-  #Hardware:           Arduino Mega 2560?                     #
+  #Hardware:           Arduino Mega 2560?      /nano          #
   #Eerste opzet:       05-02-2020                             #
-  #Auteurs: E. Hammer                                         #
+  #Auteurs: E. Hammer      N.Benhaddou                        #
   #Laatst gewijzigd:   06-02-2020                             #
   #Versie:             0.0.1                                  #
   #############################################################
@@ -30,11 +30,12 @@ struct setpoint_servowaarde {
 
 //FUNCTION PROTOTYPS
 void stuur_servo_aan (struct setpoint_servowaarde var);
-String serialInput();
+void serialInput();
+void demo(void);
 
 //VARIABLES
 String incomingByte; // for incoming serial data
-String serialInputReturn;
+
 
 void setup() {
   Serial.begin(9600);
@@ -42,28 +43,34 @@ void setup() {
   servo1.attach(s1Pin);  // attaches servo0 on pin 19 to the servo object
   servo2.attach(s2Pin);  // attaches servo0 on pin 11 to the servo object
   Serial.println("Voer servo posities in (max 180d!): s0s1s2");
+  
+  struct setpoint_servowaarde var; 
+  var.s0Pos = 65;
+  var.s1Pos = 65;
+  var.s2Pos = 65;
+  stuur_servo_aan(var);  
+  delay(10000);
 }
 
 void loop() {
   // send data only when you receive data:
-  if (Serial.available() > 0) {
-    serialInputReturn = serialInput(); 
-  }
-  
-  struct setpoint_servowaarde var;                                         //Call struct
-  var.s0Pos = serialInputReturn.substring(0,3).toInt();                    //Servo0 position in degress | Max 180
-  var.s1Pos = serialInputReturn.substring(3,6).toInt();                    //Servo1 position in degress | Max 180
-  var.s2Pos = serialInputReturn.substring(6,9).toInt();                    //Servo2 position in degress | Max 180
-  stuur_servo_aan(var);                                                    //Call function with struc variables
-
+//  if (Serial.available() > 0) {
+//    serialInput();     
+//  }
+  demo();
 }
 
-String serialInput(){
+void serialInput(){
   // read the incoming byte:
   incomingByte = Serial.readString();
   String result;
   result = incomingByte;
-  return result;
+  struct setpoint_servowaarde var;                                         //Call struct
+  var.s0Pos = result.substring(0,3).toInt();                    //Servo0 position in degress | Max 180
+  var.s1Pos = result.substring(3,6).toInt();                    //Servo1 position in degress | Max 180
+  var.s2Pos = result.substring(6,9).toInt();                    //Servo2 position in degress | Max 180
+  stuur_servo_aan(var);                                                    //Call function with struc variables
+
 }
 
 void stuur_servo_aan (struct setpoint_servowaarde var){
@@ -75,6 +82,36 @@ void stuur_servo_aan (struct setpoint_servowaarde var){
   servo1.write(var.s1Pos);           //Servo1 aansturen
   Serial.println("Server2 pos:");    //DEBUG
   Serial.println(var.s2Pos);         //DEBUG
-  servo1.write(var.s2Pos);           //Servo2 aansturen
+  servo2.write(var.s2Pos);           //Servo2 aansturen
   delay(1500);                       //DEBUG
+}
+
+void demo(void){
+  struct setpoint_servowaarde var; 
+  Serial.println("demo");//Call struct
+  var.s0Pos = 170;                 //Servo0 position in degress | Max 180
+  var.s1Pos = 170;                 //Servo1 position in degress | Max 180
+  var.s2Pos = 170;                 //Servo2 position in degress | Max 180
+  stuur_servo_aan(var);   
+                                      
+  var.s0Pos = 65;
+  var.s1Pos = 65;
+  var.s2Pos = 65;
+  stuur_servo_aan(var);  
+                                         
+  var.s0Pos = 65;
+  var.s1Pos = 165;
+  var.s2Pos = 165;
+  stuur_servo_aan(var);  
+                                           
+  var.s0Pos = 165;
+  var.s1Pos = 65;
+  var.s2Pos = 165;
+  stuur_servo_aan(var);  
+                                           
+  var.s0Pos = 165;
+  var.s1Pos = 165;
+  var.s2Pos = 65;
+  stuur_servo_aan(var);  
+
 }
